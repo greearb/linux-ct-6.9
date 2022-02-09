5c8a6c82dc88e133e203b99a25f4ddab9b7cd4f2
Ben Greear <greearb@candelatech.com>
2022-02-08 16:53:29 -0800

    ath10k: Fix db summing.
    Felix pointed out my math was wrong, so update to do a better job
    of estimating db summing.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -2713,9 +2713,9 @@ int ath10k_sum_sigs_2(int a, int b) {
 		diff = a - b;
 		if (diff == 0)
 			return a + 3;
-		else if (diff == 1)
+		else if (diff <= 2)
 			return a + 2;
-		else if (diff == 2)
+		else if (diff <= 6)
 			return a + 1;
 		return a;
 	}
@@ -2724,9 +2724,9 @@ int ath10k_sum_sigs_2(int a, int b) {
 		diff = b - a;
 		if (diff == 0)
 			return b + 3;
-		else if (diff == 1)
+		else if (diff <= 2)
 			return b + 2;
-		else if (diff == 2)
+		else if (diff <= 6)
 			return b + 1;
 		return b;
 	}
