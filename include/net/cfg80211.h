7ef8f1b7942458438e316248da9654c0682ebde9
Ben Greear <greearb@candelatech.com>
2023-08-19 09:14:26 -0700

    wifi: mac80211: Support disabling 320Mhz
    And improve some logic to disable 180Mhz for EHT connections.
    
    Signen-off-by: Ben Greear <greearb@candelatech.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -3082,6 +3082,7 @@ struct cfg80211_assoc_link {
  * @ASSOC_REQ_DISABLE_TWT:  Disable TWT
  * @ASSOC_REQ_DISABLE_160:  Disable 160Mhz
  * @ASSOC_REQ_DISABLE_OFDMA:  Disable OFDMA
+ * @ASSOC_REQ_DISABLE_320:  Disable 320Mhz
  */
 enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_HT			= BIT(0),
@@ -3095,6 +3096,7 @@ enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_TWT                   = BIT(8),
 	ASSOC_REQ_DISABLE_160			= BIT(9),
 	ASSOC_REQ_DISABLE_OFDMA			= BIT(10),
+	ASSOC_REQ_DISABLE_320			= BIT(11),
 };
 
 /**
